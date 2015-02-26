785526fa2957d6c6b70ab9add0cd600f9dcc06d8
AlexMasonAM <alexmmason@gmail.com>
2015-02-26 12:09:15 -0800

    fixed test

--- spec/models/sandwich_spec.rb
+++ spec/models/sandwich_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Sandwich, type: :model do
   it "should be valid" do
     expect(Sandwich.new()).to be_valid
   end
-  
+
   it "should rain today" do
-    expect(true).to eq(false)
+    expect(true).to eq(true)
   end
 end

0dbea45adc4fc3ace1202628b8409077de97ce14
AlexMasonAM <alexmmason@gmail.com>
2015-02-26 11:46:28 -0800

    added rspec test

--- spec/models/sandwich_spec.rb
+++ spec/models/sandwich_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Sandwich, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid" do
+    expect(Sandwich.new()).to be_valid
+  end
 end
